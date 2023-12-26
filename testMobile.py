4fb318d8a5eae72e4ab2eb7ae11b0afa95d3c245
juanmagdev <me@juanmagd.dev>
2023-12-26 15:38:27 +0100

    testing with rectangule

--- testMobile.py
+++ testMobile.py
@@ -1,13 +1,35 @@
 import cv2
-video = cv2.VideoCapture("rtsp://admin:admin@192.168.1.119:1935") # 0: webcam
+# video = cv2.VideoCapture("rtsp://admin:admin@192.168.1.119:1935") # 0: webcam
 
 
-while True:
-    ret, frame = video.read()
-    cv2.imshow('RTSP', frame)
-    k = cv2.waitKey(1)
-    if k == ord('q'):
+# while True:
+#     ret, frame = video.read()
+#     cv2.imshow('RTSP', frame)
+#     k = cv2.waitKey(1)
+#     if k == ord('q'):
+#         break
+
+# video.release()
+# cv2.destroyAllWindows()
+
+# import winsound
+cam = cv2.VideoCapture("rtsp://admin:admin@192.168.1.119:1935")
+while cam.isOpened():
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    for c in contours:
+        if cv2.contourArea(c) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # winsound.PlaySound('alert.wav', winsound.SND_ASYNC)
+    if cv2.waitKey(10) == ord('q'):
         break
-
-video.release()
-cv2.destroyAllWindows()
+    cv2.imshow('Granny Cam', frame1)
