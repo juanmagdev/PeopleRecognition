3a1f95e9898262396d89ec14be04a60267075c5f
mastercobos <manuelrr91@gmail.com>
2024-01-08 20:55:18 +0100

    Change lines 10 and 14 to working directory

--- PersonObjectsDetection/MobilNet_SSD_opencv-master/mobilenet_ssd_python.py
+++ PersonObjectsDetection/MobilNet_SSD_opencv-master/mobilenet_ssd_python.py
@@ -7,11 +7,11 @@ import cv2
 parser = argparse.ArgumentParser(
     description='Script to run MobileNet-SSD object detection network ')
 parser.add_argument("--video", help="path to video file. If empty, camera's stream will be used")
-parser.add_argument("--prototxt", default="MobileNetSSD_deploy.prototxt",
+parser.add_argument("--prototxt", default="/home/manu/Desktop/PeopleRecognition/PersonObjectsDetection/MobilNet_SSD_opencv-master/MobileNetSSD_deploy.prototxt",
                                   help='Path to text network file: '
                                        'MobileNetSSD_deploy.prototxt for Caffe model or '
                                        )
-parser.add_argument("--weights", default="MobileNetSSD_deploy.caffemodel",
+parser.add_argument("--weights", default="/home/manu/Desktop/PeopleRecognition/PersonObjectsDetection/MobilNet_SSD_opencv-master/MobileNetSSD_deploy.caffemodel",
                                  help='Path to weights: '
                                       'MobileNetSSD_deploy.caffemodel for Caffe model or '
                                       )
